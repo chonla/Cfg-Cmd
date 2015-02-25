07be217ec76e017d5487d6f5fdcd14ef57e2aec1
Chonlasith Jucksriporn <chonlasith_juc@truecorp.co.th>
2015-02-25 10:50:38 +0700

    Add options --silent, --verify, Add version to script, Create backup file before update and remove it if configuration is successfully updated.

--- cfg.php
+++ cfg.php
@@ -2,26 +2,47 @@
 
 require __DIR__.'/vendor/autoload.php';
 
+define('VERSION', '0.2');
+
 $args = \CommandLine::parseArgs($_SERVER['argv']);
 
-if (!isset($args['file'])
+if ((!isset($args['file'])
 	|| !isset($args['key'])
 	|| !isset($args['value'])
-	) {
-	echo "php cfg.php --file=<config-file> --key=<key> --value=<value> [--type=(string|number|boolean)]\n";
+	) && (!isset($args['file']) || !isset($args['verify']))) {
+	echo "Configuration Command Line Kit (version ".constant('VERSION').")\n";
+	echo "\n";
+	echo "php cfg.php --file=<config-file> --key=<key> --value=<value> [--type=(string|number|boolean)] [--silent]\n";
+	echo "php cfg.php --file=<config-file> --verify\n";
 	echo "\n";
 	echo "    --file      File name (YAML or JSON).\n";
 	echo "    --key       Key to be changed. Use dot to identify namespace.\n";
 	echo "    --value     New value.\n";
 	echo "    --type      (default=string) Type of value. If invalid type is supplied, string is used.\n";
+	echo "    --silent    Do not show update result.\n";
+	echo "    --verify    Verify the given YAML or JSON file.\n";
 	echo "\n";
 	echo "Example\n";
 	echo "    php cfg.php --file=config.yaml --key=server.host --value=localhost\n";
 	echo "    php cfg.php --file=config.yaml --key=server.port --value=80 --type=number\n";
+	echo "    php cfg.php --file=config.yaml --key=server.port --value=80 --type=number  --silent\n";
+	echo "    php cfg.php --file=config.yaml --verify\n";
 	echo "\n";
 	die();
 }
 
+// Just verify
+if (isset($args['verify'])) {
+	try {
+		$verify = \Chonla\Cfg\Config::load($args['file']);
+		echo "Configuration file is successfully verified.\n";
+	} catch(\Exception $e) {
+		die($e['message']);
+	}
+	exit(0);
+}
+
+// Update file
 if (!file_exists($args['file'])) {
 	touch ($args['file']);
 }
@@ -48,5 +69,28 @@ if (isset($args['type'])) {
 
 	}
 }
+
+// Backup
+copy($args['file'], '.'.$args['file'].'.tmp');
+
+// Do
 $config->set($args['key'], $args['value']);
 $config->save($args['file']);
+
+// Verbose
+if (!isset($args['silent'])) {
+	if ($config->get($args['key']) == $args['value']) {
+		echo "Key " . $args['key'] . " is successfully updated to " . $args['value'] . ".\n";
+	} else {
+		echo "Key " . $args['key'] . " is unable to be updated.\n";
+	}
+
+	// Verify
+	try {
+		$verify = \Chonla\Cfg\Config::load($args['file']);
+		echo "Configuration file is successfully verified.\n";
+		unlink('.'.$args['file'].'.tmp');
+	} catch(\Exception $e) {
+		die($e['message']);
+	}
+}
